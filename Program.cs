dd4282540feb5dcf4fbd54c697f119d611d8f3e0
Guilherme Fontana <gfontana3@ucs.br>
2022-12-12 09:19:02 -0300

    Adiciando notificações recusivas configuráveis

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+
+namespace DrinkWater {
+    internal class Program {
+        static void Main(string[] args) {
+            #pragma warning disable CS8604 // Possible null reference argument.
+                double interval = double.Parse(ConfigurationManager.AppSettings.Get("Interval"));
+                int limit = int.Parse(ConfigurationManager.AppSettings.Get("Limit"));
+            #pragma warning restore CS8604 // Possible null reference argument.
+
+            int i = 0;
+            while (i < limit) {
+                Notification.SendNotification("Beba água", "Esta na hora de você tomar um pouco de água");
+                Thread.Sleep(TimeSpan.FromMinutes(interval));
+                i++;
+            }
+        }
+    }
+}
---
+++ Resources/Notification.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace DrinkWater {
+    internal static class Notification {
+        public static void SendNotification(string title, string mesage) {
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText(title)
+                .AddText(mesage)
+                .Show();
+        }
+    }
+}
